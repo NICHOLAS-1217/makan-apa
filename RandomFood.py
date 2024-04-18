04cfcaa4ffc052f874b534e33bae7ca8039d5bc5
NICHOLAS-1217 <nicholastan1217@gmail.com>
2024-04-18 18:34:25 +0800

    first commit

---
+++ RandomFood.py
@@ -0,0 +1,50 @@
+import random
+import pyfiglet
+
+read = open("projects/RandomFood.txt", "r")
+data = read.read()
+
+low_cost_list = ["Hinz", "Face To Face", "Mama Homecook"]
+medium_cost_list = ["Mcdonalds", "Kung Fu", "Taco Bell", "Texas"]
+high_cost_list = ["Uncle Dons", "A&W", "Domino's Pizza", "4 Fingers"]
+all_list = [low_cost_list, medium_cost_list, high_cost_list]
+
+def random_choice(choice):
+    answer = ""
+    if choice == "low":
+        answer += random.choice(low_cost_list)
+    elif choice == "medium":
+        answer += random.choice(medium_cost_list)
+    elif choice == "high":
+        answer += random.choice(high_cost_list)
+    write = open("projects/RandomFood.txt", "w")
+    write.write(answer)
+    write.close()
+    print(answer)
+
+ascii_art = pyfiglet.figlet_format("Random Food Generator")
+print(ascii_art)
+print(f"Your last chosen restaurant was: {data}")
+choice = input("What cost would you wanna choose? (low, medium, high)\n").lower()
+
+if data in low_cost_list:
+    low_cost_list.remove(data)
+    random_choice(choice = choice)
+    low_cost_list.append(data)
+elif data in medium_cost_list:
+    medium_cost_list.remove(data)
+    random_choice(choice = choice)
+    medium_cost_list.append(data)
+elif data in high_cost_list:
+    high_cost_list.remove(data)
+    random_choice(choice = choice)
+    high_cost_list.append(data)
+
+
+
+
+
+
+
+
+
