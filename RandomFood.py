1a79397f265714e2e8e09f74f9f45109b3d02a81
NICHOLAS-1217 <nicholastan1217@gmail.com>
2024-04-20 20:41:24 +0800

    thats it for today i dont know how liao

--- RandomFood.py
+++ RandomFood.py
@@ -1,49 +1,48 @@
 import pyfiglet
 from FoodPanda import filter
 
-if __name__ == "__main__":
+# read text file
+read = open("RandomFood.txt", "r")
+data = read.read()
 
-    # read text file
-    read = open("RandomFood.txt", "r")
-    data = read.read()
+# introduction
+intro = pyfiglet.figlet_format("Makan Apa ???")
+print(intro)
+print(f"Your last chosen restaurant was: {data}")
 
-    # introduction
-    intro = pyfiglet.figlet_format("Makan Apa ???")
-    print(intro)
-    print(f"Your last chosen restaurant was: {data}")
+# inputs
+location = input("What city are you at?\n").lower()
+location = location[0].upper() + location[1:]
+budget = input("What cost would you wanna choose? (low, medium, high)\n").lower()
 
-    # inputs
-    location = input("What city are you at?\n").lower()
-    location = location[0].upper() + location[1:]
-    budget = input("What cost would you wanna choose? (low, medium, high)\n").lower()
+cost = 0
+if budget == "low":
+    cost = 1
+elif budget == "medium":
+    cost = 2
+elif budget == "high":
+    cost = 3
 
-    cost = 0
-    if budget == "low":
-        cost = 1
-    elif budget == "medium":
-        cost = 2
-    elif budget == "high":
-        cost = 3
+filter(location = location, cost = cost)
 
-    filter(location = location, cost = cost)
+is_good = False
 
-    is_good = False
+try_time = 0
 
-    try_time = 0
-
-    while is_good == False:
-        result = input("Is this good for you? (y/n)\n")
-        if result == "n":
-            filter(location = location, cost = cost)
-            is_good = False
-            try_time += 1
-            if try_time > 3:
-                print(pyfiglet.figlet_format("You are picky eater, no need to eat"))
-                is_good = True
-        else:
-            print("let's goooo!!!")
+while is_good == False:
+    result = input("Is this good for you? (y/n)\n")
+    if result == "n":
+        filter(location = location, cost = cost)
+        is_good = False
+        try_time += 1
+        if try_time > 3:
+            print(pyfiglet.figlet_format("You are picky eater, no need to eat"))
             is_good = True
-
+            break
+    else:
+        print("let's goooo!!!")
+        is_good = True
+        break
 
 
 
